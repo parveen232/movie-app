4013f95aca7158628e0821193bcdb83cb8fcb897
parveen232 <parveen297m210@gmail.com>
2023-03-06 11:52:33 +0530

    home page - request popular movies

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+import { api_key } from "../../../config";
+
 export default function Home() {
-  return <h1>This is Home Page for movie app</h1>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    requestMovies();
+  }, []);
+
+  async function requestMovies() {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
+    );
+    const data = await res.json();
+    setMovies(data.results);
+  }
+
+  return (
+    <>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <h2>{movie.title}</h2>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
